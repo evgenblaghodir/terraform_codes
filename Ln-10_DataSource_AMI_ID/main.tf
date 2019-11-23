93adbb1a67f0aa6705ef783e3540bdc85eaa59ae
Evgen Blaghodir <e.blaghodir@gmail.com>
2019-11-23 23:35:07 +0200

    Ln10 added resource Ubuntu

--- Ln-10_DataSource_AMI_ID/main.tf
+++ Ln-10_DataSource_AMI_ID/main.tf
@@ -11,6 +11,26 @@ data "aws_ami" "latest_ubuntu_server" {
   }
 }
 
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "my_Ubuntu" {
+  count         = 1
+  ami           = data.aws_ami.latest_ubuntu_server.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name    = "my_Ubuntu_server"
+    Owner   = "Yevhen"
+    Project = "Terraform Lesson"
+  }
+}
 
 output "latest_ubuntu_ami_id" {
   value = data.aws_ami.latest_ubuntu_server.id
